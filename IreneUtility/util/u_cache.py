85554270c2c2125b872547d0b81e0f13e676bea3
Mujy <thugystory1@gmail.com>
2021-06-01 12:29:00 -0400

    Added dead image cache as a metric

--- IreneUtility/util/u_cache.py
+++ IreneUtility/util/u_cache.py
@@ -724,7 +724,8 @@ class Cache(Base):
                     'total_amount_of_self_assignable_roles': sum([len(channel_and_roles.get('roles') or [])
                                                                   for channel_and_roles in
                                                                   self.ex.cache.assignable_roles.values()]),
-                    'channels_with_games_disabled': len(self.ex.cache.channels_with_disabled_games)
+                    'channels_with_games_disabled': len(self.ex.cache.channels_with_disabled_games),
+                    'dead_image_cache': len(self.ex.cache.dead_image_cache)
                 }
 
                 # set all per minute metrics to 0 since this is a 60 second loop.
