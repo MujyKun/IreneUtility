1746d763abb21f9d8ff266f93f4f494b64cfd707
Mujy <thugystory1@gmail.com>
2021-07-20 23:45:52 -0400

    Added method to fetch user wins

--- IreneUtility/s_sql/s_biasgame.py
+++ IreneUtility/s_sql/s_biasgame.py
@@ -1,1 +1,7 @@
 from . import self
+
+
+async def fetch_user_wins(user_id: int):
+    """Fetch a user's bias game scores with the idol id to the score."""
+    return await self.conn.fetch("SELECT idolid, wins FROM biasgame.winners WHERE userid = $1 ORDER BY WINS DESC",
+                                 user_id)
