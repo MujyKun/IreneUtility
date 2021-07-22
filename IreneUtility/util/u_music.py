48ed904d2c4961d736c8799582c83525048e085e
Mujy <thugystory1@gmail.com>
2021-07-22 12:50:25 -0400

    Updating to 1.0 wavelink

--- IreneUtility/util/u_music.py
+++ IreneUtility/util/u_music.py
@@ -1,12 +1,14 @@
-from typing import Union, Optional
+from typing import Union, Optional, List
 
 import asyncio
 
 import aiohttp.client_exceptions
 import wavelink
 import discord
+import re
+import traceback
 from discord.ext import commands
-
+from wavelink.ext import spotify
 from ..Base import Base
 from . import u_logger as log
 
@@ -27,7 +29,7 @@ class LoopController(Base):
 
     async def controller_loop(self):
         """Manages when to play the next song."""
-        player = self.ex.wavelink.get_player(self.guild_id)
+        player = await self.ex.u_music.get_guild_player(self.guild_id)
         await player.set_volume(self.volume)
 
         while True:
@@ -47,7 +49,7 @@ class LoopController(Base):
 
             if not played:  # no songs are queued.
                 await player.disconnect(force=True)
-                await player.destroy(force=True)
+                await player.cleanup()
                 break
 
             await self.next.wait()
@@ -59,19 +61,35 @@ class Music(Base):
     def __init__(self, *args):
         super().__init__(*args)
         self.controllers = {}  # guild_id: controller
+        self.node_pool = wavelink.NodePool()
+        self.removing_partial_tracks = False
+        self.URL_REGEX = re.compile(r'https?://(?:www\.)?.+')
 
     async def start_nodes(self):
         """Initiate the wavelink nodes."""
         for voice_region in self.ex.cache.voice_regions:
             try:
                 log.console(f"Attempting to Start Wavelink node for {voice_region}.", method=self.start_nodes)
-                await wavelink.NodePool.create_node(bot=self.ex.client, bot_id=self.ex.keys.bot_id, region=voice_region,
-                                                    identifier=voice_region, spotify_client=self.ex.spotify_client,
-                                                    **self.ex.keys.wavelink_options)
+                await self.node_pool.create_node(bot=self.ex.client, bot_id=self.ex.keys.bot_id, region=voice_region,
+                                                 identifier=voice_region, spotify_client=self.ex.spotify_client,
+                                                 **self.ex.keys.wavelink_options)
             except aiohttp.client_exceptions.ClientConnectionError:
                 log.console(f"Failed to initiate a node for {voice_region}.")
             except Exception as e:
                 log.console(e, method=self.start_nodes)
+
+    async def get_guild_player(self, guild: Union[discord.Guild, int]):
+        """Get a player for a guild if it exists."""
+        if isinstance(guild, int):
+            guild = self.ex.client.get_guild(guild)
+
+        if not guild:
+            return
+
+        for node in self.node_pool.nodes.values():
+            player = node.get_player(guild)
+            if player:
+                return player
 
     def __destroy_controller(self, controller: LoopController):
         """Removes a controller from the collection of controllers if it existed."""
@@ -88,12 +106,6 @@ class Music(Base):
         await player.disconnect(force=True)
         await player.destroy(force=True)
 
-    async def on_event_hook(self, event):
-        """Node hook callback."""
-        if isinstance(event, (wavelink.TrackEnd, wavelink.TrackException)):
-            controller = self.get_controller(event.player)
-            controller.next.set()
-
     def get_controller(self, value: Union[commands.Context, wavelink.Player], create_new=True):
         """Get the controller of a guild.
 
@@ -102,10 +114,7 @@ class Music(Base):
         :param value: Context or a Player.
         :param create_new: Whether to create a new instance if one is not found.
         """
-        if isinstance(value, commands.Context):
-            guild_id = value.guild.id
-        else:
-            guild_id = value.guild_id
+        guild_id = value.guild.id
         controller = self.controllers.get(guild_id)
         if not controller and create_new:
             controller = LoopController(guild_id, self.ex)
@@ -142,8 +151,8 @@ class Music(Base):
             ctx = track.info.get("ctx")
             if ctx:
                 msg = await self.ex.get_msg(ctx, "music", "now_playing", [
-                    ["title", track.title],
-                    ["artist", track.author]
+                    ["title", player.source.info.get("title")],
+                    ["artist", player.source.info.get("author")]
                 ])
                 await ctx.send(msg)
             return True
@@ -155,7 +164,7 @@ class Music(Base):
         :param pause: Whether to pause.
         :returns: Wavelink Player
         """
-        player = self.ex.wavelink.get_player(ctx.guild.id)
+        player = await self.get_guild_player(ctx.guild)
 
         if not player.is_connected:
             await ctx.invoke(self.connect_to_vc(ctx))
@@ -178,13 +187,15 @@ class Music(Base):
 
         :raises: (discord.NotFound) Author is not in a voice channel or a general exception occurred.
         """
+
         async def connect(current_channel=None):
             if current_channel:
                 await current_channel.disconnect()
 
-            player: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+            voice_channel = ctx.author.voice.channel
+            player: wavelink.Player = await voice_channel.connect(cls=wavelink.Player)
             await ctx.send(await self.ex.get_msg(ctx, "music", "connecting", ["voice_channel",
-                                                                              ctx.author.voice_channel.name]))
+                                                                              voice_channel.name]))
             return player
 
         try:
@@ -215,7 +226,7 @@ class Music(Base):
         page_number = 1
         if hasattr(player, "playlist"):
             # get the track currently playing
-            current_track: wavelink.Track = player.current
+            current_track: wavelink.Track = player.source
             if current_track:
                 queue_desc += f"NOW PLAYING: {await self.get_track_info(current_track)}\n"
                 # Currently playing song does not count as a queue index.
@@ -237,20 +248,126 @@ class Music(Base):
 
         return embed_list
 
-    async def get_track_info(self, track: wavelink.Track):
+    async def get_track_info(self, track: Union[wavelink.Track, wavelink.PartialTrack]):
         """
         Puts Track into a displayable form for displaying a queue.
 
         :param track: Wavelink Track.
         :returns: (str) Message containing the title, artist, duration, and mention of user that requested the song.
         """
-        song_info = f"**{track.title}** by **{track.author}** (" \
-                    f"**{await self.ex.u_miscellaneous.get_cooldown_time(track.length//1000)}**)"
+        extra_info = ""
+        if isinstance(track, wavelink.Track):
+            extra_info = f" by **{track.author}** " \
+                         f"(**{await self.ex.u_miscellaneous.get_cooldown_time(track.length)}**)"
+        song_info = f"**{track.title}**{extra_info}"
         ctx = track.info.get("ctx")
         if ctx:
             song_info += f" - Requested by <@{ctx.author.id}>"
         song_info += "\n"
         return song_info
 
+    async def search_query(self, search_query: str):
+        """Search Tracks for youtube/spotify/soundcloud playlists/tracks based on a search query.
 
+        :param search_query: Query to search tracks for.
+        """
 
+        # currently disabled soundcloud due to possibly
+        # searching spotify instead of soundcloud in wavelink beta version.
+        # soundcloud_tracks = await self.__search_soundcloud(search_query)
+
+        spotify_tracks = await self.__search_spotify(search_query, playlist=True) or await self.__search_spotify(
+            search_query, album=True) or await self.__search_spotify(search_query)
+
+        youtube_tracks = await self.__search_youtube(search_query)
+
+        return spotify_tracks or youtube_tracks
+
+    async def __search_youtube(self, search_query: str) \
+            -> Optional[Union[List[wavelink.PartialTrack], List[wavelink.Track]]]:
+        """Search Partial/Tracks for youtube based on a search query.
+
+        :param search_query: Query to search tracks for.
+        :returns: Optional[Union[List[wavelink.PartialTrack], List[wavelink.Track]]]
+        """
+        search_query = search_query.strip('<>')
+        get_first_result = self.URL_REGEX.match(search_query)
+
+        if get_first_result:
+            # search Youtube Music
+            tracks = await wavelink.YouTubeMusicTrack.search(query=search_query, return_first=get_first_result)
+            if tracks:
+                return tracks
+
+        # search regular youtube.
+        tracks = await wavelink.YouTubeTrack.search(query=search_query, return_first=get_first_result)
+        if tracks:
+            return tracks
+
+    async def __search_soundcloud(self, search_query: str) \
+            -> Optional[Union[List[wavelink.PartialTrack], List[wavelink.Track]]]:
+        """Search Partial/Tracks for soundcloud based on a search query.
+
+        :param search_query: Query to search tracks for.
+        :returns: Optional[Union[List[wavelink.PartialTrack], List[wavelink.Track]]]
+        """
+        # only get first result
+        tracks = await wavelink.SoundCloudTrack.search(query=search_query)
+        if tracks:
+            return [tracks]
+
+    async def __search_spotify(self, search_query: str, playlist=False, album=False) \
+            -> Optional[Union[List[wavelink.PartialTrack], List[spotify.SpotifyTrack]]]:
+        """Search PartialTracks for spotify playlists based on a search query.
+
+        :param search_query: Query to search tracks for.
+        :param playlist: (bool) To search for playlists.
+        :param album: (bool) To search for albums.
+        :returns: Optional[List[wavelink.PartialTrack]]
+        """
+        tracks = []
+
+        try:
+            if playlist:
+                type = spotify.SpotifySearchType.playlist
+            elif album:
+                type = spotify.SpotifySearchType.album
+            else:
+                type = spotify.SpotifySearchType.track
+                track = await spotify.SpotifyTrack.search(query=search_query, return_first=True)
+                if track:
+                    return [track]
+
+            iterator = spotify.SpotifyTrack.iterator(query=search_query, type=type, partial_tracks=True)
+            async for track in iterator:
+                if not hasattr(track, "info"):
+                    track.info = {}
+                tracks.append(track)
+        except TypeError:
+            pass
+        except spotify.SpotifyRequestError:
+            pass
+        except:
+            traceback.print_exc()
+
+        return tracks
+
+    async def remove_partial_tracks(self, player):
+        """Remove partial tracks and convert them to regular tracks."""
+        if self.removing_partial_tracks:
+            return
+
+        self.removing_partial_tracks = True
+        if not hasattr(player, "playlist"):
+            return
+
+        # do not want to enumerate since we need to confirm index at the moment the track is being set.
+        for track in player.playlist:
+            if isinstance(track, spotify.PartialTrack):
+                try:
+                    player.playlist[player.playlist.index(track)] = await track._search()
+                except ValueError:  # not in list
+                    pass
+                except IndexError:  # incorrect index
+                    pass
+        self.removing_partial_tracks = False
