94671c2a706f0288e4261ad4be29ac0987cbad73
Mujy <thugystory1@gmail.com>
2021-08-10 13:12:57 -0400

    send images to host is on by default

--- IreneUtility/util/u_groupmembers.py
+++ IreneUtility/util/u_groupmembers.py
@@ -1089,8 +1089,7 @@ class GroupMembers(Base):
             await self.add_idol_to_group(idol_obj.id, group_id)
             idol_obj.groups.append(group_id)
 
-        if not self.ex.test_bot:
-            await self.fix_links()
+        await idol_obj.send_images_to_host()
         return idol_obj
 
     async def add_new_group(self, group_name, *args) -> models.Group:
@@ -1103,8 +1102,7 @@ class GroupMembers(Base):
         group_obj = await self.add_group_to_cache(
             **(await self.ex.sql.s_groupmembers.fetch_latest_group(group_name)))
 
-        if not self.ex.test_bot:
-            await self.fix_links()
+        await group_obj.send_images_to_host()
         return group_obj
 
     async def fix_links(self):
